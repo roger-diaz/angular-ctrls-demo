03776f8908d304610158a8e8680998b712b0d02a
Roger Diaz Benavides <roger.diaz@rivetlogic.com>
2013-06-18 10:05:48 -0600

    Add main files

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+function CStudioFormController($scope){
+	$scope.fields = [ 
+	{ id: 'file-name', value: '/site/website/test.xml', type: 'file-name'}, 
+	{ id: 'internal-name', value: 'Test', type: 'input'} ];
+}
